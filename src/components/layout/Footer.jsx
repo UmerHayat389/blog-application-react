26e81ea358e306d0acb29bf8f41994578adca777
UmerHayat389 <walterhenry5535@gmail.com>
2026-01-03 21:25:14 +0500

    React Blog App

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
 return (
 <footer className='bg-gray-900 text-gray-300 py-10 mt-16'>
 <div className='max-w-7xl mx-auto px-6 text-center'>
-<p>© 2026 BlogSphere. All rights reserved.</p>
+<p>© 2026 Umer Hayat. All rights reserved.</p>
 </div>
 </footer>
 )
